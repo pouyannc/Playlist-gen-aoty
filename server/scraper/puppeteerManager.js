5b3ce2772b4dc2c081877780a826775f4b528ae0
Pouyan <pouyannc@gmail.com>
2023-12-05 22:37:35 -0500

    revert for heroku deploy

--- server/scraper/puppeteerManager.js
+++ server/scraper/puppeteerManager.js
@@ -7,39 +7,45 @@ class PuppeteerManager {
   }
 
   async runPuppeteer() {
-    const chromium = require('@sparticuz/chromium-min');
+    // const chromium = require('@sparticuz/chromium-min');
     const puppeteer = require('puppeteer-extra');
 
-    require('puppeteer-extra-plugin-stealth/evasions/chrome.app');
-    require('puppeteer-extra-plugin-stealth/evasions/chrome.csi');
-    require('puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes');
-    require('puppeteer-extra-plugin-stealth/evasions/chrome.runtime');
-    require('puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow');
-    require('puppeteer-extra-plugin-stealth/evasions/media.codecs');
-    require('puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency');
-    require('puppeteer-extra-plugin-stealth/evasions/navigator.languages');
-    require('puppeteer-extra-plugin-stealth/evasions/navigator.permissions');
-    require('puppeteer-extra-plugin-stealth/evasions/navigator.plugins');
-    require('puppeteer-extra-plugin-stealth/evasions/navigator.vendor');
-    require('puppeteer-extra-plugin-stealth/evasions/navigator.webdriver');
-    require('puppeteer-extra-plugin-stealth/evasions/sourceurl');
-    require('puppeteer-extra-plugin-stealth/evasions/user-agent-override');
-    require('puppeteer-extra-plugin-stealth/evasions/webgl.vendor');
-    require('puppeteer-extra-plugin-stealth/evasions/window.outerdimensions');
-    require('puppeteer-extra-plugin-stealth/evasions/defaultArgs');
-    require('puppeteer-extra-plugin-user-preferences');
-    require('puppeteer-extra-plugin-user-data-dir');
+    // require('puppeteer-extra-plugin-stealth/evasions/chrome.app');
+    // require('puppeteer-extra-plugin-stealth/evasions/chrome.csi');
+    // require('puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes');
+    // require('puppeteer-extra-plugin-stealth/evasions/chrome.runtime');
+    // require('puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow');
+    // require('puppeteer-extra-plugin-stealth/evasions/media.codecs');
+    // require('puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency');
+    // require('puppeteer-extra-plugin-stealth/evasions/navigator.languages');
+    // require('puppeteer-extra-plugin-stealth/evasions/navigator.permissions');
+    // require('puppeteer-extra-plugin-stealth/evasions/navigator.plugins');
+    // require('puppeteer-extra-plugin-stealth/evasions/navigator.vendor');
+    // require('puppeteer-extra-plugin-stealth/evasions/navigator.webdriver');
+    // require('puppeteer-extra-plugin-stealth/evasions/sourceurl');
+    // require('puppeteer-extra-plugin-stealth/evasions/user-agent-override');
+    // require('puppeteer-extra-plugin-stealth/evasions/webgl.vendor');
+    // require('puppeteer-extra-plugin-stealth/evasions/window.outerdimensions');
+    // require('puppeteer-extra-plugin-stealth/evasions/defaultArgs');
+    // require('puppeteer-extra-plugin-user-preferences');
+    // require('puppeteer-extra-plugin-user-data-dir');
 
     const StealthPlugin = require('puppeteer-extra-plugin-stealth')
     puppeteer.use(StealthPlugin())
 
+    // const browser = await puppeteer.launch({
+    //   args: [...chromium.args, '--hide-scrollbars', '--disable-web-security', "--no-sandbox"],
+    //   executablePath: await chromium.executablePath(
+    //     'https://github.com/Sparticuz/chromium/releases/download/v119.0.0/chromium-v119.0.0-pack.tar'
+    //   ),
+    //   headless: chromium.headless,
+    //   ignoreHTTPSErrors: true,
+    //   dumpio: false,
+    // });
+
     const browser = await puppeteer.launch({
-      args: [...chromium.args, '--hide-scrollbars', '--disable-web-security', "--no-sandbox"],
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath(
-        'https://github.com/Sparticuz/chromium/releases/download/v119.0.0/chromium-v119.0.0-pack.tar'
-      ),
-      headless: chromium.headless,
+      args: ['--hide-scrollbars', '--disable-web-security', "--no-sandbox", '--disable-gpu'],
+      headless: true,
       ignoreHTTPSErrors: true,
       dumpio: false,
     });
