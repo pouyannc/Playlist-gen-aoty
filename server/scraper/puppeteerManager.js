46375e0027501ca3d8ebd56f01e1f04f871bd78f
Pouyan <pouyannc@gmail.com>
2023-12-05 21:56:37 -0500

    '

--- server/scraper/puppeteerManager.js
+++ server/scraper/puppeteerManager.js
@@ -10,25 +10,25 @@ class PuppeteerManager {
     const chromium = require('@sparticuz/chromium-min');
     const puppeteer = require('puppeteer-extra');
 
-    // require('puppeteer-extra-plugin-stealth/evasions/chrome.app');
-    // require('puppeteer-extra-plugin-stealth/evasions/chrome.csi');
-    // require('puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes');
-    // require('puppeteer-extra-plugin-stealth/evasions/chrome.runtime');
-    // require('puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow');
-    // require('puppeteer-extra-plugin-stealth/evasions/media.codecs');
-    // require('puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency');
-    // require('puppeteer-extra-plugin-stealth/evasions/navigator.languages');
-    // require('puppeteer-extra-plugin-stealth/evasions/navigator.permissions');
-    // require('puppeteer-extra-plugin-stealth/evasions/navigator.plugins');
-    // require('puppeteer-extra-plugin-stealth/evasions/navigator.vendor');
-    // require('puppeteer-extra-plugin-stealth/evasions/navigator.webdriver');
-    // require('puppeteer-extra-plugin-stealth/evasions/sourceurl');
-    // require('puppeteer-extra-plugin-stealth/evasions/user-agent-override');
-    // require('puppeteer-extra-plugin-stealth/evasions/webgl.vendor');
-    // require('puppeteer-extra-plugin-stealth/evasions/window.outerdimensions');
-    // require('puppeteer-extra-plugin-stealth/evasions/defaultArgs');
-    // require('puppeteer-extra-plugin-user-preferences');
-    // require('puppeteer-extra-plugin-user-data-dir');
+    require('puppeteer-extra-plugin-stealth/evasions/chrome.app');
+    require('puppeteer-extra-plugin-stealth/evasions/chrome.csi');
+    require('puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes');
+    require('puppeteer-extra-plugin-stealth/evasions/chrome.runtime');
+    require('puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow');
+    require('puppeteer-extra-plugin-stealth/evasions/media.codecs');
+    require('puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency');
+    require('puppeteer-extra-plugin-stealth/evasions/navigator.languages');
+    require('puppeteer-extra-plugin-stealth/evasions/navigator.permissions');
+    require('puppeteer-extra-plugin-stealth/evasions/navigator.plugins');
+    require('puppeteer-extra-plugin-stealth/evasions/navigator.vendor');
+    require('puppeteer-extra-plugin-stealth/evasions/navigator.webdriver');
+    require('puppeteer-extra-plugin-stealth/evasions/sourceurl');
+    require('puppeteer-extra-plugin-stealth/evasions/user-agent-override');
+    require('puppeteer-extra-plugin-stealth/evasions/webgl.vendor');
+    require('puppeteer-extra-plugin-stealth/evasions/window.outerdimensions');
+    require('puppeteer-extra-plugin-stealth/evasions/defaultArgs');
+    require('puppeteer-extra-plugin-user-preferences');
+    require('puppeteer-extra-plugin-user-data-dir');
 
     const StealthPlugin = require('puppeteer-extra-plugin-stealth')
     puppeteer.use(StealthPlugin())
